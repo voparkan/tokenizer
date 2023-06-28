6d8ac7b7a5b6f71cbf6fdfd6e835221874badf6f
voparkan <voparkan@student.42heilbronn.de>
2023-06-28 03:24:23 +0200

    creating files

--- tokenizer/src/main.rs
+++ tokenizer/src/main.rs
@@ -9,17 +9,23 @@ use daemonize::Daemonize;
 use std::path::Path;
 use std::fs::File;
 
-fn main() {
+fn main()
+{
     let run = true;
-    let homedir = Path::new(dirs::home_dir());
-    let stdout = File::create(homedir.join("/tokenizer/daemonizer.out")).unwrap();
-    let stderr = File::create(homedir.join("/tokenizer/daemonizer.err")).unwrap();
-    File::create(homedir.join("/tokenizer/daemonizer.pid"));
-
+    let home_dir = dirs::data_dir().expect("REASON");
+    let home_dir_path = Path::new(&home_dir);
+    let tokenizer_path = home_dir_path.join("tokenizer");
+    let stdout_path = tokenizer_path.join("daemonizer.out");
+    let stderr_path = tokenizer_path.join("daemonizer.err");
+    let pid_path = tokenizer_path.join("daemonizer.pid");
+    let _dir = std::fs::create_dir_all(&tokenizer_path).expect("tokenizer dir was not created");
+    let stdout = File::create(&stdout_path).unwrap();
+    let stderr = File::create(&stderr_path).unwrap();
+    let pid_file = File::create(&pid_path);
     let tokenizer = Daemonize::new()
-        .pid_file(homedir.join("/tokenizer/daemonizer.pid")) // Every method except `new` and `start`
+        .pid_file(pid_path) // Every method except `new` and `start`
         .chown_pid_file(false)      // is optional, see `Daemonize` documentation
-        .working_directory(homedir.join("/tokenizer")) // for default behaviour.
+        .working_directory(&tokenizer_path) // for default behaviour.
         .user("nobody")
         .group("nobody") // Group name
         .group(2)        // or group id.
@@ -28,40 +34,22 @@ fn main() {
         .stderr(stderr)  // Redirect stderr to `/tmp/daemon.err`.
         .privileged_action(|| "Executed before drop privileges");
 
-    match tokenizer.start() {
+    println!("pid_file {:#?}", pid_file);
+    match pid_file
+    {
         Ok(_) => {
-            while run {
-                eprintln!("Hello, world! {}", dirs::home_dir().expect("expected home directory").display());
+            match tokenizer.start() {
+                Ok(ok) => {
+                    eprintln!("TOKENIZER! \n\nHome directory: {}  \n\nPid file: {}", stdout_path.display(), ok);
+                }
+                Err(e) => eprintln!("TOKENIZER! \n\nError: {} \ndir: {}", e, stderr_path.display()),
             }
         }
-        Err(e) => eprintln!("Error {}", e),
+        Err(ref error) => {
+            panic!("Enviroment not ready {} \ndir: {:#?}", error, pid_file);
+        }
+    }
+    while run {
+        
     }
 }
-
-// fn main()
-// {
-//     let run = true;
-//     let stdout = File::create("~/.tokenizer/daemonizer.out"); //.join(".tokenizer").join("daemonizer.out")
-//     let stderr = File::create("~/.tokenizer/daemonizer.err"); //.join(".tokenizer").join("daemonizer.err")
-//
-//     let daemon = Daemonize::new()
-//         .pid_file("~/.tokenizer/tokenizer.pid") //dirs::home_dir().join(".tokenizer").join("tokenizer.pid")
-//         .chown_pid_file(true)
-//         .working_directory("~/.tokenizer") //dirs::home_dir().join(".tokenizer")
-//         .user("tokenizer")
-//         .group("daemon")
-//         .group(2)
-//         .umask(0o777)
-//         .stdout(stdout)
-//         .stderr(stderr)
-//         .privileged_action(|| "Executed before drop priviledges");
-//
-//     match daemon.start() {
-//         Ok(_) => {
-//             while run {
-//                 println!("Hello, world!");
-//             }
-//         }
-//         Err(e) => eprintln!("Error {}", e),
-//     }
-// }
